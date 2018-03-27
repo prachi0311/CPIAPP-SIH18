5c062a98d1fb9fcaeecc45c9e845d16c588910ec
Prachi Singh <nero11223344@gmail.com>
2018-03-27 22:42:16 +0530

    shop schedule added

---
+++ app/src/main/java/com/example/prachisingh/cpi_ur/Adapters/ShopListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.prachisingh.cpi_ur.Adapters;
+
+/**
+ * Created by prachisingh on 27/03/18.
+ */
+
+public class ShopListAdapter {
+}
---
+++ app/src/main/java/com/example/prachisingh/cpi_ur/ApiResponses/ShopScheduleResponse.java
@@ -0,0 +1,8 @@
+package com.example.prachisingh.cpi_ur.ApiResponses;
+
+/**
+ * Created by prachisingh on 27/03/18.
+ */
+
+public class ShopScheduleResponse {
+}
